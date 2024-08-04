2cbf916f78c3756dc69d6dfff4d88b69e82a18d6
spahicadis <adis@spahica.com>
2024-08-04 12:11:28 +0200

    Finished calculating and displaying data

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
-import Toastify from 'toastify-js';
-import "toastify-js/src/toastify.css";
+//import Toastify from 'toastify-js';
+//import "toastify-js/src/toastify.css";
+
+//DOM
+const billAmount = document.querySelector('.bill');
+const tipPercentage = document.querySelector('.tip');
+const calculateButton = document.querySelector('.buttonAction');
+const result = document.querySelector('.calculatedAmount');
+
+
+//STATEs
+let bill = 0;
+let tip = 0;
+let calculatedAmount = 0;
+
+//BINDING
+billAmount.addEventListener('input', function(e) {
+    bill = e.target.value;
+    console.log(typeof bill);
+})
+
+tipPercentage.addEventListener('input', function(e) {
+    tip = e.target.value;
+    console.log(typeof tip);
+})
+
+//CALCULATE
+function calculate() {
+ let formula = (parseFloat(tip) / 100) * parseFloat(bill) + parseFloat(bill);
+ result.innerHTML = formula
+}
+
+calculateButton.addEventListener('click', calculate);
+
