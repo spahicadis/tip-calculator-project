3f7dff48e7e66fe499a5cc484578c76f43fc3c05
spahicadis <adis@spahica.com>
2024-08-28 16:44:21 +0200

    DRY KONCEPT

--- app.js
+++ app.js
@@ -13,25 +13,30 @@ let bill = 0;
 let tip = 0;
 let calculatedAmount = 0;
 
+//DRY
+function displayToastify(text, gravity, position) {
+  Toastify({
+    text: text,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: gravity, // `top` or `bottom`
+    position: position, // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function(){} // Callback after click
+  }).showToast();
+}
+
 //BINDING
 billAmount.addEventListener('input', function(e) {
     bill = e.target.value;
     console.log(bill);
     if (parseFloat(bill) < 0) {
-        Toastify({
-            text: "The value cannot be negative",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();
+       displayToastify('The value cannont be negative', 'top', 'center')
     }
 })
 
@@ -39,20 +44,7 @@ tipPercentage.addEventListener('input', function(e) {
     tip = e.target.value;
     console.log(tip);
     if (parseFloat(tip) < 0) {
-        Toastify({
-            text: "The value cannont be negative",
-            duration: 3000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "center", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function(){} // Callback after click
-          }).showToast();  
+       displayToastify('The value cannont be negative', 'top', 'center')
     }
 
 })
@@ -62,24 +54,12 @@ function calculate() {
 calculatedAmount = (parseFloat(tip) / 100) * parseFloat(bill) + parseFloat(bill);
  result.innerHTML = calculatedAmount;
  if(calculatedAmount < 0) {
-    Toastify({
-        text: "Not possbile",
-        duration: 3000,
-        destination: "https://github.com/apvarun/toastify-js",
-        newWindow: true,
-        close: true,
-        gravity: "bottom", // `top` or `bottom`
-        position: "center", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-          background: "linear-gradient(to right, #00b09b, #96c93d)",
-        },
-        onClick: function(){} // Callback after click
-      }).showToast();
+   displayToastify('Not possible', 'bottom', 'center')
       calculatedAmount = 0
       result.innerHTML = calculatedAmount
  }
 }
 
 calculateButton.addEventListener('click', calculate);
+
 
